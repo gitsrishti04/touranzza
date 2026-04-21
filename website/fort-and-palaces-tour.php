b51b9f0932867a1391ec7fc1f1dfa982a0effaec
Srishti khandelwal <srishti@Srishtis-MacBook-Air.local>
2026-04-21 20:49:44 +0530

    Fix 27 broken internal images and generate missing Kerala tour image

--- website/fort-and-palaces-tour.php
+++ website/fort-and-palaces-tour.php
@@ -610,7 +610,7 @@
     <!-- /.mobile-nav__overlay -->
     <div class="mobile-nav__content"> <span class="mobile-nav__close mobile-nav__toggler"><i
           class="fa fa-times"></i></span>
-      <div class="logo-box"> <a href="./" aria-label="logo image"><img src="assets/images/logo-dark.png"
+      <div class="logo-box"> <a href="./" aria-label="logo image"><img src="assets/images/blacklogo1.png"
             width="155" alt="" /></a> </div>
       <!-- /.logo-box -->
       <div class="mobile-nav__container"></div>
